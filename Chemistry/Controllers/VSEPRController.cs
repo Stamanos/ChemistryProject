1837a559ab5ef5270d1a79f2502c67ac2dd0e1c7
Manos Stamatakis <themagicsport@gmail.com>
2020-03-04 12:52:59 +0200

    install the kekule library for the 3D chemistry display

---
+++ Chemistry/Controllers/VSEPRController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chemistry.Controllers
+{
+    public class VSEPRController : Controller
+    {
+        // GET: VSEPR
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: VSEPR/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VSEPR/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VSEPR/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VSEPR/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VSEPR/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VSEPR/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VSEPR/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
